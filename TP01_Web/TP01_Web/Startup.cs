953d33826b24337f743fdaf770ee982713fb0bcb
kevpug <pugk0305@gmail.com>
2021-03-25 19:16:14 -0400

    Header/Footer style

--- TP01_Web/TP01_Web/Startup.cs
+++ TP01_Web/TP01_Web/Startup.cs
@@ -27,7 +27,7 @@ namespace TP01_Web
             services.AddScoped<IDépôt, DépôtDéveloppement>();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+        public void Configure(IApplicationBuilder app) {
 
             app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
