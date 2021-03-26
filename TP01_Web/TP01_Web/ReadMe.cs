20a74b20371785479866fa5ddd94f04e2770d520
Arnaud Labrecque <70404494+ArnaudLabTech@users.noreply.github.com>
2021-03-26 01:23:28 -0400

    Readme

--- TP01_Web/TP01_Web/ReadMe.cs
+++ TP01_Web/TP01_Web/ReadMe.cs
@@ -5,8 +5,27 @@ using System.Threading.Tasks;
 
 namespace TP01_Web
 {
-    public class ReadMe
+    public interface ReadMe
     {
-
+        /*
+         * Créateurs
+         * Arnaud Labrecque : 1679694
+         * Kevin Pugliese : 1793507
+         * SDK 3.1.407 
+         * Bootstrap 4.6.0
+         * FontAwesome 4.7.0
+         * 
+         * Cette application web est le squelette pour une compagnie de location de voitures. Un utilisateur se connecte
+         * et selon ses permissions, peut ajouter un autre utilisateur à l'application (réservé aux administrateurs) ou rien faire. 
+         * Les administrateurs peuvent seulement ajouter un commis ou un gérant.
+         * 
+         * Projet en Développement, en ce moment la vérification de connexion est basé sur une variable static booléenne 
+         * et devrait changer dans le futur.
+         * 
+         * Cette application est remis en format compressé .zip. Il faut le décompresser et ouvrir la solution afin de voir 
+         * et tester le code. Touche F5 pour partir en mode debug une fois la solution ouverte.
+         * 
+         * 
+         */
     }
 }
