33d1f3b5858690478125a45c795ab8eb6e4c23bc
Arnaud Labrecque <70404494+ArnaudLabTech@users.noreply.github.com>
2021-04-29 14:26:33 -0400

    Key

--- TP_Web/TP_Web/Models/Succursale.cs
+++ TP_Web/TP_Web/Models/Succursale.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace TP_Web.Models
 {
     public sealed class Succursale
     {
+        [Key]
         public long SuccursaleId { get; set; }
         public long NuméroCivique { get; set; }
         public string NomRue { get; set; }
--- TP_Web/TP_Web/Models/Voiture.cs
+++ TP_Web/TP_Web/Models/Voiture.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,6 +14,7 @@ namespace TP_Web.Models
         Luxe
         }
 
+        [Key]
         public long VoitureId { get; set; }
         public string Modèle { get; set; }
         public int Année { get; set; }
