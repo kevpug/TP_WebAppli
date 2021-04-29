360463593ac8a94d20fcdfadcc304fc9e9ed5408
kevpug <pugk0305@gmail.com>
2021-04-29 03:31:30 -0400

    xd

---
+++ TP_Web/TP_Web/Models/PeuplerIdentité.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TP_Web.Models
+{
+    public class PeuplerIdentité
+    {
+        public static void CréerCompteAdminDéfault(IServiceProvider p_serviceProvider,
+                                                   IConfiguration p_configuration)
+        {
+            //CréerCompteAdminDéfaultAsync(p_serviceProvider, p_configuration).Wait();
+        }
+
+        //public static async Task CréerCompteAdminDéfaultAsync(IServiceProvider p_serviceProvider, IConfiguration p_configuration)
+        //{
+        //    await new NotImplementedException();
+        //}
+    }
+}
--- TP_Web/TP_Web/Startup.cs
+++ TP_Web/TP_Web/Startup.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TP_Web.Models;
 using TP01_Web.Models;
 
 namespace TP01_Web
@@ -16,6 +19,29 @@ namespace TP01_Web
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ContexteIdentité>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("ChainesConnexion:ConnexionIdentite")));
+            services.AddDbContext<ContexteLocoAuto>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("ChainesConnexion:ConnexionLocoAuto")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>(opts =>
+            {
+                opts.User.RequireUniqueEmail = true;
+                opts.Password.RequiredLength = 6;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+            })
+                    .AddEntityFrameworkStores<ContexteIdentité>();
+
+            //Page par défault pour l'authentification
+            services.ConfigureApplicationCookie(options =>
+                options.LoginPath = "/Utilisateur/Authentification");
+            // Page par défault pour l'accès refusé
+            services.ConfigureApplicationCookie(options =>
+                options.AccessDeniedPath = "/Utilisateur/AccèsRefusé");
+
             services.AddControllersWithViews();
             services.AddSingleton<IDépôt, DépôtDéveloppement>(); //Singleton pour qu'il soit la même liste pour le site au complet.
         }
@@ -26,11 +52,14 @@ namespace TP01_Web
             app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}");
+                endpoints.MapControllerRoute("controllers",
+                "controllers/{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapDefaultControllerRoute();
+                endpoints.MapRazorPages();
             });
         }
     }
