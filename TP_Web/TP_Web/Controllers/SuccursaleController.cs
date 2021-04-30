de07dfa5ae0125d3b4a3346916004ccfd5f86b57
Arnaud Labrecque <70404494+ArnaudLabTech@users.noreply.github.com>
2021-04-29 23:35:03 -0400

    Controller/model Succursale almost done

--- TP_Web/TP_Web/Controllers/SuccursaleController.cs
+++ TP_Web/TP_Web/Controllers/SuccursaleController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using TP_Web.Models;
+using System.Linq;
 
 namespace TP_Web.Controllers
 {
@@ -8,6 +9,11 @@ namespace TP_Web.Controllers
     {
 
         private IDépôt dépôt;
+
+        public SuccursaleController(IDépôt p_dépôt)
+        {
+            dépôt = p_dépôt;
+        }
 
         public IActionResult Index()
         {
@@ -26,6 +32,30 @@ namespace TP_Web.Controllers
         public async Task<IActionResult> AjouterSuccursale(Succursale p_succursale)
         {
             ViewBag.Noms = "Arnaud Labrecque & Kevin Pugliese";
+
+            if (dépôt.Succursales.Any(s => s.SuccursaleId == p_succursale.SuccursaleId))
+                ModelState.AddModelError(nameof(Succursale.SuccursaleId), "Ce numéro de Succursale existe déjà!");
+
+            if (dépôt.Succursales.Any(s => s.NomRue == p_succursale.NomRue && s.CodePostal == p_succursale.CodePostal))
+            { 
+                ModelState.AddModelError(nameof(Succursale.NomRue), "Il ne peut pas y avoir de Succursale à la même rue et code postal");
+                ModelState.AddModelError(nameof(Succursale.CodePostal), "Il ne peut pas y avoir de Succursale à la même rue et code postal");
+            }
+
+            if (dépôt.Succursales.Any(s => s.NomVille != p_succursale.NomVille && s.CodePostal == p_succursale.CodePostal))
+            {
+                ModelState.AddModelError(nameof(Succursale.NomVille), "Il ne peut pas y avoir de Succursale à la même rue et code postal");
+                ModelState.AddModelError(nameof(Succursale.CodePostal), "Il ne peut pas y avoir de Succursale à la même rue et code postal");
+            }
+
+            if (dépôt.Succursales.Any(s => s.NomProvince != p_succursale.NomProvince && s.CodePostal == p_succursale.CodePostal))
+            {
+                ModelState.AddModelError(nameof(Succursale.NomProvince), "Il ne peut pas y avoir de Succursale à la même province et code postal");
+                ModelState.AddModelError(nameof(Succursale.CodePostal), "Il ne peut pas y avoir de Succursale à la même province et code postal");
+            }
+
+
+
 
             if (ModelState.IsValid)
             {
--- TP_Web/TP_Web/Models/Succursale.cs
+++ TP_Web/TP_Web/Models/Succursale.cs
@@ -9,12 +9,25 @@ namespace TP_Web.Models
     public sealed class Succursale
     {
         [Key]
-        public long SuccursaleId { get; set; }
-        public long NuméroCivique { get; set; }
+        [Required(ErrorMessage = "Veuillez fournir un numéro de succursale")]
+        [Range(0, long.MaxValue,
+        ErrorMessage = "Le numéro de voiture doit être un chiffre positif.")]
+        public long? SuccursaleId { get; set; }
+        [Required(ErrorMessage = "Veuillez fournir un numéro civique")]
+        [Range(0, long.MaxValue,
+        ErrorMessage = "Le numéro de voiture doit être un chiffre positif.")]
+        public long? NuméroCivique { get; set; }
+        [Required(ErrorMessage = "Veuillez fournir un nom de rue")]
         public string NomRue { get; set; }
+        [Required(ErrorMessage = "Veuillez fournir un nom de ville")]
         public string NomVille { get; set; }
+        [Required(ErrorMessage = "Veuillez fournir un nom de province")]
         public string NomProvince { get; set; }
+        [Required(ErrorMessage = "Veuillez fournir un code postal")]
+        [RegularExpression(@"^[a-zA-Z][0-9][a-zA-Z][0-9][a-zA-Z][0-9]$", ErrorMessage = "Veuillez fournir un code postal dans un format valide (LCLCLC).")]
         public string CodePostal { get; set; }
+        [Required(ErrorMessage = "Veuillez fournir un numéro de téléphone")]
+        [RegularExpression(@"^[\d][\d][\d][\d][\d][\d][\d][\d][\d][\d]", ErrorMessage = "Veuillez fournir un numéro de téléphone valide")]
         public string NuméroTéléphone { get; set; }
 
 
