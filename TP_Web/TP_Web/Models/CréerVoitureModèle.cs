30d19a85d1acc2a4a1fac7207ed530b45ac592b3
Arnaud Labrecque <70404494+ArnaudLabTech@users.noreply.github.com>
2021-04-30 21:47:59 -0400

    Voiture controller test

--- TP_Web/TP_Web/Models/CréerVoitureModèle.cs
+++ TP_Web/TP_Web/Models/CréerVoitureModèle.cs
@@ -6,26 +6,16 @@ namespace TP_Web.Models
     public sealed class CréerVoitureModèle : ReadMe
     {
         [Required(ErrorMessage = "Veuillez entrer un numéro de voiture.")]
-        [Range(0, long.MaxValue,
-        ErrorMessage = "Le numéro de la voiture doit être un chiffre positif.")]
         public long? NuméroVoiture { get; set; }
         [Required(ErrorMessage = "Veuillez entrer un modèle de voiture.")]
         public string Modèle { get; set; }
         [Required(ErrorMessage = "Veuillez entrer une année.")]
-        [Range(1900, 9999,
-        ErrorMessage = "L'année doit être entre {1} et {2}.")]
         public int? Année { get; set; }
         [Required(ErrorMessage = "Veuillez sélectionner un groupe.")]
         public GroupeVoiture? Groupe { get; set; }
-
         [Required(ErrorMessage = "Veuillez entrer le millage.")]
-        [Range(0, long.MaxValue,
-        ErrorMessage = "Le millage doit être un chiffre positif.")]
         public long? Millage { get; set; }
-
         [Required(ErrorMessage = "Veuillez entrer le numéro de la succursale.")]
-        [Range(0, long.MaxValue,
-        ErrorMessage = "Le numéro de la succursale doit être un chiffre positif.")]
         public long? Succursale { get; set; }
     }
 }
