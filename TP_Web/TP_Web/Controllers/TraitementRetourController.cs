4493d17d486b1c180ac8a53fb455e04a6969be52
Arnaud Labrecque <70404494+ArnaudLabTech@users.noreply.github.com>
2021-05-13 17:47:30 -0400

    Fini home Index

---
+++ TP_Web/TP_Web/Controllers/TraitementRetourController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TP_Web.Models;
+
+namespace TP_Web.Controllers
+{
+    public class TraitementRetourController : Controller
+    {
+        private IDépôt dépôt;
+
+        public TraitementRetourController(IDépôt p_dépôt)
+        {
+            dépôt = p_dépôt;
+        }
+
+        public IActionResult RetournerVoiture()
+        {
+            ViewBag.Noms = "Arnaud Labrecque & Kevin Pugliese";
+            return View();
+        }
+    }
+}
