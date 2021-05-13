3fe79a1933378b395525d107195d3be352a18824
Arnaud Labrecque <70404494+ArnaudLabTech@users.noreply.github.com>
2021-05-13 17:38:42 -0400

    Home, manque interface traitement de retour et fermerdossier

--- TP_Web/TP_Web/Controllers/LocationController.cs
+++ TP_Web/TP_Web/Controllers/LocationController.cs
@@ -3,14 +3,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TP_Web.Models;
 
 namespace TP_Web.Controllers
 {
     public class LocationController : Controller
     {
-        public IActionResult Index()
+        private IDépôt dépôt;
+
+        public LocationController(IDépôt p_dépôt)
         {
+            dépôt = p_dépôt;
+        }
+
+
+        public IActionResult LouerVoiture()
+        {
+            ViewBag.Noms = "Arnaud Labrecque & Kevin Pugliese";
             return View();
         }
+
     }
 }
